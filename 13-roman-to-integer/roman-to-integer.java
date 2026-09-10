c0b0995757edaa44db6e6d58c295eca12a5d6864
Gourav prakash <gouravmehta9905@gmail.com>
2026-09-10 14:57:35 +0530

    Time: 5 ms (31.25%) | Memory: 46.8 MB (27.52%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -9,18 +9,13 @@ class Solution {
         roman.put('C', 100);
         roman.put('D', 500);
         roman.put('M', 1000);
-
-        // Loop through the string, except the last character
         for (int i = 0; i < s.length() - 1; i++) {
-            // Subtract if current value is less than next value
             if (roman.get(s.charAt(i)) < roman.get(s.charAt(i + 1))) {
                 res -= roman.get(s.charAt(i));
             } else {
-                // Otherwise, add the value
                 res += roman.get(s.charAt(i));
             }
         }
-        // Add the value of the last character
         return res + roman.get(s.charAt(s.length() - 1)); 
     }
 }
